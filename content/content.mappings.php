1450d54c88ac81babb43dede8f52c8dd3be72e78
Brendan Abbott <brendan@bloodbone.ws>
2014-01-31 09:10:37 +1000

    Show JSON file if the Section has spaces

--- content/content.mappings.php
+++ content/content.mappings.php
@@ -51,7 +51,7 @@
 			parent::view(FALSE);
 
 			if(isset($this->mode)) {
-				$section = $this->mode;
+				$section = preg_replace('/-/', '_', $this->mode);
 				header('Content-Type: application/json');
 				echo file_get_contents(WORKSPACE . '/elasticsearch/mappings/' . $section . '.json');
 				die;
